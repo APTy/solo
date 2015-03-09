22466561ab6b1dfde492e7c01ce987593a6828a7
R. Tyler Julian <trjulian@gmail.com>
2015-03-09 16:25:01 -0700

    Implement basic text-to-speech

--- public/stout.js
+++ public/stout.js
@@ -1,10 +1,42 @@
 var app = angular.module('stout', []);
 
+var play = (function() {
+  var queue = [];
+
+  var add = function(text) {
+    if (queue.indexOf(text) === -1) {
+      queue.push(text);
+
+      if (queue.length === 1) {
+        play();
+      }
+    }
+  }
+
+  var play = function() {
+    var text = queue[0];
+
+    var msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+  }
+
+  return {
+    add: add
+  };
+})();
+
 app.controller('MainController', function($scope, $http) {
   var data = JSON.stringify({url: 'testing'});
 
+  $scope.play = function($event) {
+    var text = $event.target.innerText;
+    play.add(text);
+  };
+
   $http.post('http://localhost:3000/api', data)
     .success(function(data) {
+      document.getElementById('title').innerHTML = data.title;
+      document.getElementById('author').innerHTML = data.author;
       document.getElementById('content').innerHTML = data.content;
     });
 });
