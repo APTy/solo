d439020fb04aba0db4d22c0793b83be742ae0a87
R. Tyler Julian <trjulian@gmail.com>
2015-03-10 15:49:23 -0700

    Display loading message to user and title for news feed

--- public/app/menu/menu.js
+++ public/app/menu/menu.js
@@ -1,28 +1,7 @@
 angular.module('stout.menu', [])
 
 .controller('MenuController', function ($scope, $http, Menu) {
-  $scope.news = [
-    {title: 'Dow sharply lower amid rate-hike fears',
-    description: 'NEW YORK (MarketWatch) — U.S. stocks fell sharply on Tuesday with main indexes declining more than 1%.'
-    },
-
-    {title: 'Sony confirms PS4 is heading to China',
-      description: 'Sony has announced it\'s planning to launch the PS4 in China following the country\'s decision to lift its 14-year console ban.'
-    },
-
-    {title: 'Starbucks expands its mobile ordering service',
-      description: 'Previously only available in Portland, Oregon, Starbucks is now bringing its mobile ordering and payments program to 650 locations in the Pacific Northwest.'
-    },
-
-    {title: 'Europe puts a cap on credit and debit card fees',
-      description: 'On a continent where there are 1.5 plastic cards for every person, every penny saved counts.'
-    },
-
-    {title: 'The Wikimedia Foundation is suing the NSA',
-      description: 'The nonprofit behind Wikipedia—the world’s encyclopedia—takes issue with both the US National Security Agency.'
-    }
-
-  ];
+  $scope.news = [{description: 'loading content'}];
 
   $scope.go = function() {
     if ($scope.form.$valid) {
