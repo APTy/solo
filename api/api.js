d7bd7b299034280f923028cf4c81a80ac9f3b6dc
R. Tyler Julian <trjulian@gmail.com>
2015-03-10 15:40:24 -0700

    Refactor server API requests into encapsulated module

---
+++ api/api.js
@@ -0,0 +1,32 @@
+// api.js
+
+var request = require('request');
+var config = require('./env/config.js');
+
+exports.getReadability = function(url, cb) {
+  var api = 'https://readability.com/api/content/v1/parser';
+  // var url = 'http://blog.readability.com/2011/02/step-up-be-heard-readability-ideas';
+  var token = config.readToken;
+  var uri = api + '?url=' + url + '&token=' + token;
+
+  request({
+      uri: uri,
+      method: "GET",
+      timeout: 10000
+    }, function(err, res, body) {
+      var data = JSON.parse(body);
+      cb(data);
+    });
+};
+
+exports.getNews = function(cb) {
+  var uri = 'http://api.usatoday.com/open/articles/topnews/home?count=10&days=0&page=0&encoding=json&api_key=ch5b5vgs4h3wr4a93e3jasf7';
+  request({
+      uri: uri,
+      method: "GET",
+      timeout: 10000
+    }, function(err, res, body) {
+      var data = JSON.parse(body);
+      cb(data);
+    });
+};
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// server.js
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var api = require('./api/api.js');
 
 var request = require('request');
 var config = require('./env/config.js');
@@ -21,33 +24,33 @@ app.get('/news', function (req, res) {
 })
 
 
-var getReadability = function(url, cb) {
-  var api = 'https://readability.com/api/content/v1/parser';
-  // var url = 'http://blog.readability.com/2011/02/step-up-be-heard-readability-ideas';
-  var token = config.readToken;
-  var uri = api + '?url=' + url + '&token=' + token;
-
-  request({
-      uri: uri,
-      method: "GET",
-      timeout: 10000
-    }, function(err, res, body) {
-      var data = JSON.parse(body);
-      cb(data);
-    });
-};
-
-var getNews = function(cb) {
-  var uri = 'http://api.usatoday.com/open/articles/topnews/home?count=10&days=0&page=0&encoding=json&api_key=ch5b5vgs4h3wr4a93e3jasf7';
-  request({
-      uri: uri,
-      method: "GET",
-      timeout: 10000
-    }, function(err, res, body) {
-      var data = JSON.parse(body);
-      cb(data);
-    });
-};
+// var getReadability = function(url, cb) {
+//   var api = 'https://readability.com/api/content/v1/parser';
+//   // var url = 'http://blog.readability.com/2011/02/step-up-be-heard-readability-ideas';
+//   var token = config.readToken;
+//   var uri = api + '?url=' + url + '&token=' + token;
+//
+//   request({
+//       uri: uri,
+//       method: "GET",
+//       timeout: 10000
+//     }, function(err, res, body) {
+//       var data = JSON.parse(body);
+//       cb(data);
+//     });
+// };
+//
+// var getNews = function(cb) {
+//   var uri = 'http://api.usatoday.com/open/articles/topnews/home?count=10&days=0&page=0&encoding=json&api_key=ch5b5vgs4h3wr4a93e3jasf7';
+//   request({
+//       uri: uri,
+//       method: "GET",
+//       timeout: 10000
+//     }, function(err, res, body) {
+//       var data = JSON.parse(body);
+//       cb(data);
+//     });
+// };
 
 getNews();
 
